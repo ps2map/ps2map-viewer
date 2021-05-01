6ece9e2dd43773c831d769831baf5899dd61e07a
Leonhard S <leonhard-sei@outlook.com>
2021-05-01 18:27:06 +0200

    Add two-part base icons
    These use the foreground outlines from the corresponding SVGs while generating the background colour in CSS. This ensures that the SVGs can be efficiently cached and also facilitates updating the base colours according to capture status.

--- src/layers/baseNameLayer.ts
+++ src/layers/baseNameLayer.ts
@@ -24,19 +24,28 @@ class BaseNameLayer extends MapLayer {
         bases.then((bases) => {
             const elements: Array<HTMLDivElement> = [];
             bases.forEach((base) => {
-                const container = document.createElement("div");
+                const anchor = document.createElement("div");
                 const offsetX = (4096 + base.map_pos[0]) / 81.92;
                 const offsetY = (4096 + base.map_pos[1]) / 81.92;
-                container.style.left = `${offsetX}%`;
-                container.style.bottom = `${offsetY}%`;
-                let icon = document.createElement("object");
-                icon.setAttribute("data", "img/icons/warp-gate.svg");
-                icon.setAttribute("type", "image/svg+xml");
-                container.appendChild(icon);
-                let name = document.createElement("span");
+                anchor.setAttribute("class", "mapAnchor");
+                anchor.style.left = `${offsetX}%`;
+                anchor.style.bottom = `${offsetY}%`;
+                // Base icon
+                const iconBox = document.createElement("div");
+                anchor.appendChild(iconBox);
+                iconBox.setAttribute("class", "iconBox");
+                const layerImage = document.createElement("div");
+                iconBox.appendChild(layerImage);
+                layerImage.setAttribute("class", "layeredIcon");
+                const icon = document.createElement("img");
+                layerImage.appendChild(icon);
+                icon.setAttribute("alt", "Amp Station");
+                icon.setAttribute("src", "img/icons/amp-station.svg");
+                const name = document.createElement("p");
+                anchor.appendChild(name);
+                name.setAttribute("class", "baseLabel");
                 name.innerHTML = base.name;
-                container.appendChild(name);
-                elements.push(container);
+                elements.push(anchor);
             });
             this.clear();
             elements.forEach((element) => this.layer.appendChild(element));
