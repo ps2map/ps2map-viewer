62f0f8feaef802461a09d8db0287c054558b5e96
Leonhard S <leonhard-sei@outlook.com>
2022-07-31 16:11:00 +0200

    Add colour picker

--- index.js
+++ index.js
@@ -1540,7 +1540,7 @@ var Brush = (function (_super) {
         this._cursor.style.borderRadius = Brush.size * 0.5 + "px";
     };
     Brush.prototype._action = function (context, pos, scale) {
-        context.fillStyle = "rgb(255, 255, 0)";
+        context.fillStyle = Brush.color;
         context.beginPath();
         context.arc(pos.x, pos.y, Brush.size * scale * 0.5, 0, 2 * Math.PI, false);
         context.fill();
@@ -1549,10 +1549,21 @@ var Brush = (function (_super) {
         _super.prototype._setUpToolPanel.call(this);
         var frag = document.createDocumentFragment();
         frag.appendChild(document.createTextNode("Hold LMB to draw, MMB to pan"));
+        frag.appendChild(document.createElement("br"));
+        frag.appendChild(document.createTextNode("Color:"));
+        var picker = document.createElement("input");
+        picker.type = "color";
+        picker.value = "#ffff00";
+        picker.style.margin = "10px";
+        picker.addEventListener("change", function () {
+            Brush.color = picker.value;
+        });
+        frag.appendChild(picker);
         this._tool_panel.appendChild(frag);
         this._tool_panel.style.display = "block";
     };
     Brush.size = 10;
+    Brush.color = "rgb(255, 255, 0)";
     Brush.id = "brush";
     Brush.displayName = "Brush";
     return Brush;
--- src/toolbox/brush.ts
+++ src/toolbox/brush.ts
@@ -3,6 +3,7 @@
 class Brush extends CanvasTool {
 
     static size = 10;
+    static color = "rgb(255, 255, 0)";
 
     static readonly id = "brush";
     static readonly displayName = "Brush";
@@ -19,7 +20,7 @@ class Brush extends CanvasTool {
         pos: Point,
         scale: number,
     ): void {
-        context.fillStyle = "rgb(255, 255, 0)";
+        context.fillStyle = Brush.color;
         context.beginPath();
         context.arc(pos.x, pos.y, Brush.size * scale * 0.5, 0, 2 * Math.PI, false);
         context.fill();
@@ -30,6 +31,16 @@ class Brush extends CanvasTool {
 
         const frag = document.createDocumentFragment();
         frag.appendChild(document.createTextNode("Hold LMB to draw, MMB to pan"));
+        frag.appendChild(document.createElement("br"));
+        frag.appendChild(document.createTextNode("Color:"));
+        const picker = document.createElement("input");
+        picker.type = "color";
+        picker.value = "#ffff00";
+        picker.style.margin = "10px";
+        picker.addEventListener("change", () => {
+            Brush.color = picker.value;
+        });
+        frag.appendChild(picker)
         this._tool_panel.appendChild(frag);
         this._tool_panel.style.display = "block";
     }
